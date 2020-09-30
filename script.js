21fd7bea2c114050d90e6e47daef51b26fa55940
muborizMirzoev <muboriz10@gmail.com>
2020-09-30 18:58:29 +0700

    динамическая появление формы

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// const form = document.querySelector('.form');
-// const lid = document.querySelector('.lid');
+const form = document.querySelector('.form');
+const lid = document.querySelector('.lid');
 
-// form.style.display = 'none';
+form.style.display = 'none';
 
-// lid.addEventListener('click', function() {
-//   if(form.style.display == 'none') form.style.display = 'block';
-//   else form.style.display = 'none';
-// });
+lid.addEventListener('click', function() {
+  if(form.style.display == 'none') form.style.display = 'block';
+  else form.style.display = 'none';
+});
