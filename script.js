147a6c7c328c52f6d45db7fd8841585bcf186410
muborizMirzoev <muboriz10@gmail.com>
2020-10-01 14:50:11 +0700

    Добавил страницу спасибо

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ lid.addEventListener('click', function() {
   if(form.style.display == 'none') form.style.display = 'block';
   else form.style.display = 'none';
 });
+
+
+// Страница спасибо после отравки формы
+
+$(document).ready(function() {
+  var $form = $("#form");
+  $form.submit(function() {
+    $.ajax({
+      type: "POST",
+      url: "send.php",
+      data: $(this).serialize()
+    //}).done(function() {
+    }).fail(function() {
+      $form.css("display", "none");
+      $(".center").css("display", "none");
+      $(".thanks").css("display", "block");
+    });
+    return false;
+  });
+});
